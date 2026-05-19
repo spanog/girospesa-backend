7516c93565516038db7a32815c64dd4c89997df1
spanog <spn.giacomo@gmail.com>
2026-05-19 20:02:33 +0200

    apply some fixes

--- core/database.py
+++ core/database.py
@@ -9,14 +9,8 @@ import psycopg2.extras
 from supabase import create_client, Client
 from core.config import settings
 
-_client: Client | None = None
-
-
 def get_supabase() -> Client:
-    global _client
-    if _client is None:
-        _client = create_client(settings.supabase_url, settings.supabase_service_role_key)
-    return _client
+    return create_client(settings.supabase_url, settings.supabase_service_role_key)
 
 
 def get_database_dsn() -> str:
